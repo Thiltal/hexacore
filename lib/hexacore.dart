5eb404e89ec4caac76e3803f4994e5ddddcebfe8
nachvilku01@centrum.cz <mlcoch.zdenek@seznam.cz>
2014-09-21 18:58:27 +0200

    setup

---
+++ lib/hexacore.dart
@@ -0,0 +1,14 @@
+library hexacore;
+
+part 'src/map.dart';
+part 'src/field.dart';
+part 'src/unit.dart';
+part 'src/ability.dart';
+part 'src/item.dart';
+part 'src/action.dart';
+part 'src/flora.dart';
+part 'src/world.dart';
+
+main(){
+  
+}
---
+++ test/hexacore_text.dart
@@ -0,0 +1,66 @@
+ library hexacore_test;  
+   
+ import 'package:hexacore/hexacore.dart';  
+ import 'package:unittest/unittest.dart';
+ import 'dart:math';
+ 
+ main() {
+  
+  test("distance", (){
+    expect(CircleSector.distance(0, 1, 1, 1), equals(1));
+    expect(CircleSector.distance(0, 1, 2, 2), equals(2));
+    expect(CircleSector.distance(0, 1, 1, 2), equals(2));
+    expect(CircleSector.distance(0, 1, 1, 3), equals(3));
+    expect(CircleSector.distance(0, 1, 0, 4), equals(3));
+    expect(CircleSector.distance(3, 3, 0, 4), equals(3));
+    expect(CircleSector.distance(3, 3, 2, 4), equals(1));
+    expect(CircleSector.distance(3, 3, 2, 3), equals(1));
+    expect(CircleSector.distance(3, 3, 4, 3), equals(1));
+    expect(CircleSector.distance(3, 3, 3, 2), equals(1));
+    expect(CircleSector.distance(3, 3, 4, 4), equals(1));
+    
+    int iterations = 10000000;
+    
+    int numbers = iterations*4;
+    
+    Random r = new Random(333);
+    List<int> test = new List(numbers);
+    for(int i=0;i<numbers;i++){
+      test[i] = r.nextInt(500);
+    }
+    Stopwatch sw1 = new Stopwatch()..start();
+        for(int i=0;i<numbers;i+=4){
+              test[i];
+              test[i+1];
+              test[i+2];
+              test[i+3];
+         }
+    sw1.stop();
+    
+    Stopwatch sw = new Stopwatch()..start();
+    for(int i=0;i<numbers;i+=4){
+          CircleSector.distance(test[i], test[i+1], test[i+2], test[i+3]);
+     }
+    print("distances per second ${(1000/(sw.elapsedMilliseconds-sw1.elapsedMilliseconds)*iterations~/1000000).toInt()}M");
+    
+  });
+  
+  test("isInMap", (){
+    
+  });
+  
+  test("createMap",(){
+    CircleSector cs = new CircleSector(2, 2, 2);
+    expect(cs.fields.first.x, equals(0));
+    expect(cs.fields.first.y, equals(1));
+    
+    cs = new CircleSector(1, 1, 1);
+    expect(cs.fields.first.x, equals(0));
+    expect(cs.fields.first.y, equals(1));
+    
+    cs = new CircleSector(3, 2, 3);
+    expect(cs.fields.first.x, equals(0));
+    expect(cs.fields.first.y, equals(1)); 
+  });
+  
+ } 
